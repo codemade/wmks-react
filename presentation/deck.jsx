a4d36c08eb2e83c49e852c28d15c6b99800a9a17
Christian Kühl <kuehlchristian@icloud.com>
2015-10-19 18:19:52 +0200

    Edit first slide.

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -12,7 +12,11 @@ import Interactive from "./interactive";
 const images = {
   city: require("./city.jpg"),
   kat: require("./kat.png"),
-  logo: require("./formidable-logo.svg")
+  react: require("./react.svg"),
+  chrkhl: require("./chrkhl.png"),
+  revrng: require("./revrng.png"),
+  twitter: require("./twitter.svg"),
+  github: require("./github.svg")
 };
 
 preloader([images.city, images.kat]);
@@ -22,19 +26,11 @@ export default class extends React.Component {
     return (
       <Deck transition={["zoom", "slide"]} transitionDuration={800}>
         <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="black">
-            Spectacle
+          <Heading size={2} textSize="12em" lineHeight={0.5} textColor="secondary">
+            React JS
           </Heading>
-          <Heading size={1} fit caps>
-            A ReactJS Presentation Library
-          </Heading>
-          <Heading size={1} fit caps textColor="black">
-            Where You Can Write Your Decks In JSX
-          </Heading>
-          <Link href="https://github.com/FormidableLabs/spectacle">
-            <Text bold caps textColor="tertiary">View on Github</Text>
-          </Link>
-          <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
+          <Image src={images.react} height="300px"/>
+          <Text bold caps textColor="tertiary">Webmontag Kassel, 09. November 2015</Text>
         </Slide>
         <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
           <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
